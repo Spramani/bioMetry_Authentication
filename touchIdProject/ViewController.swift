6075aa3a260a74f37431a7f9f03b907fd900883a
shubham P Ramani <shubhamramani903@gmail.com>
2021-08-23 10:14:59 +0530

    new

--- touchIdProject/ViewController.swift
+++ touchIdProject/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import LocalAuthentication
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -14,6 +16,96 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func authBtn(_ sender: Any) {
+//        let context = LAContext()
+//        var error:NSError? = nil
+//
+//        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+//            let reson = "Identify yourself"
+//
+//            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reson){ [weak self]
+//                success, AuthenticationError in
+//
+//
+//                DispatchQueue.main.async {
+//                    if success {
+//                        let ac = UIAlertController(title: "Authentication Success", message: "You could be verified; Thank you.", preferredStyle: .alert)
+//                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+//                        self?.present(ac, animated: true)
+//                    } else {
+//                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+//                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+//                        self!.present(ac, animated: true)
+//                    }
+//                }
+//            }
+//        }else {
+//            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+//            ac.addAction(UIAlertAction(title: "OK", style: .default))
+//            self.present(ac, animated: true)
+//        }
+//
+//
+        let localAuthenticationContext = LAContext()
+        localAuthenticationContext.localizedFallbackTitle = "Please use your Passcode"
 
+        var authorizationError: NSError?
+        let reason = "Authentication is required for you to continue"
+
+        if localAuthenticationContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthentication, error: &authorizationError) {
+            
+            let biometricType = localAuthenticationContext.biometryType == LABiometryType.faceID ? "Face ID" : "Touch ID"
+            print("Supported Biometric type is: \( biometricType )")
+            
+            localAuthenticationContext.evaluatePolicy(LAPolicy.deviceOwnerAuthentication, localizedReason: reason) { (success, evaluationError) in
+                if success {
+                    print("Success")
+                } else {
+                    print("Error \(evaluationError!)")
+                    if let errorObj = evaluationError {
+                        let messageToDisplay = self.getErrorDescription(errorCode: errorObj._code)
+                        print(messageToDisplay)
+                    }
+                }
+            }
+              
+        } else {
+            print("User has not enrolled into using Biometrics")
+        }
+    }
+    
+    func getErrorDescription(errorCode: Int) -> String {
+     
+        switch errorCode {
+            
+        case LAError.authenticationFailed.rawValue:
+            return "Authentication was not successful, because user failed to provide valid credentials."
+            
+        case LAError.appCancel.rawValue:
+            return "Authentication was canceled by application (e.g. invalidate was called while authentication was in progress)."
+            
+        case LAError.invalidContext.rawValue:
+            return "LAContext passed to this call has been previously invalidated."
+            
+        case LAError.notInteractive.rawValue:
+            return "Authentication failed, because it would require showing UI which has been forbidden by using interactionNotAllowed property."
+            
+        case LAError.passcodeNotSet.rawValue:
+            return "Authentication could not start, because passcode is not set on the device."
+            
+        case LAError.systemCancel.rawValue:
+            return "Authentication was canceled by system (e.g. another application went to foreground)."
+            
+        case LAError.userCancel.rawValue:
+            return "Authentication was canceled by user (e.g. tapped Cancel button)."
+            
+        case LAError.userFallback.rawValue:
+            return "Authentication was canceled, because the user tapped the fallback button (Enter Password)."
+            
+        default:
+            return "Error code \(errorCode) not found"
+        }
+        
+     }
 }
 
